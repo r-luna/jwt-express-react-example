680a5931593d7a04c927d6d88d58254000349d05
Ricardo Luna <rluna@lilly.com>
2019-08-22 12:38:47 -0400

    add jwt functions

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,13 @@
 const exists = require('./user/exists');
 const authorize = require('./user/authorize');
 const dropCookie = require('./user/dropCookie');
+const { jwtSign, jwtVerify, jwtDecode } = require('./user/jwt');
 
-module.exports = { exists, authorize, dropCookie };
+module.exports = {
+  exists,
+  authorize,
+  dropCookie,
+  jwtSign,
+  jwtVerify,
+  jwtDecode,
+};
