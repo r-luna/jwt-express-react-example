f27d1daeed2fc25bd3a0e85b820bd52539621549
Ricardo Luna <rluna@lilly.com>
2019-08-22 12:39:19 -0400

    add cookies

--- middleware/user/dropCookie.js
+++ middleware/user/dropCookie.js
@@ -1,13 +1,19 @@
 
 const dropCookie = () => {
-  return (req, res, next) => {
-    const { exists, authorized } = res.locals.user;
-    if (exists && authorized) {
-
+  return async (req, res, next) => {
+    const { exists, authorized, jwt } = res.locals.user;
+    if (exists && authorized && jwt) {
+      try {
+        await res.cookie('jwt', jwt, { httpOnly: true, expires: new Date(Date.now() + (60000) * process.env.JWT_EXP) });
+      } catch(err) {
+        res.locals.user.cookieDropped = false;
+      }
+      res.locals.user.cookieDropped = true;
     } else {
-      
+      res.locals.user.cookieDropped = false;
     }
-  }
+    next();
+  };
 };
 
 module.exports = dropCookie;
