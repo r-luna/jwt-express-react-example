e43ebf1aaee040d31d9aa480f41284436044b6ca
Ricardo Luna <rluna@lilly.com>
2019-08-22 12:40:11 -0400

    add middlewares

---
+++ routes/user/login.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const {
+  exists,
+  authorize,
+  dropCookie,
+  jwtSign,
+} = require('../../middleware');
+
+const router = express.Router();
+
+// validate user and issue a JWT via an httponly cookie
+router.post('/login', exists(), authorize(), jwtSign(), dropCookie(), (req, res, next) => {
+  let response;
+  if (res.locals.user.cookieDropped) {
+    const { username, email, role } = res.locals.user;
+    res.status(200);
+    response = {
+      status: 'success',
+      message: 'Login Success',
+      user: {
+        username,
+        email,
+        role,
+      },
+    };
+  } else {
+    res.status(401);
+    response = { status: 'failure', message: 'Unauthorized' };
+  }
+  res.json(response);
+});
+
+module.exports = router;
