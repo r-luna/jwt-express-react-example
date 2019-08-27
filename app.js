c087fc75b621ead28635f6a12340ed0437052d91
Ricardo Luna <rluna@lilly.com>
2019-08-27 13:45:00 -0400

    remove duplicative stuff

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ const corsConfig = {
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
